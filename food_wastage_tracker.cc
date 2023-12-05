bd8113baf3cccd77263db8b4e23d2964df7b96c0
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2023-12-05 21:51:32 +0000

    Code updates

--- food_wastage_tracker.cc
+++ food_wastage_tracker.cc
@@ -18,3 +18,30 @@
 // to tell the compiler that each function belongs to the FoodWastageTracker
 // class.
 // ===================================================================
+FoodWastageTracker::FoodWastageTracker() {}
+
+bool FoodWastageTracker::AddRecord(FoodWastageRecord record) {
+  for (FoodWastageRecord& r : records_) {
+    if (r.Equals(record)) {
+      return false;
+    }
+  }
+  records_.push_back(record);
+  return true;
+}
+bool FoodWastageTracker::RemoveRecord(FoodWastageRecord record) {
+  int i;
+  for (i = 0; i < records_.size(); i++) {
+    if (records_[i].Equals(record)) {
+      break;
+    }
+  }
+  if (i < records_.size()) {
+    records_.erase(records_.begin() + i);
+    return true;
+  }
+  return false;
+}
+const std::vector<FoodWastageRecord>& FoodWastageTracker::GetRecords() const {
+  return records_;
+}
